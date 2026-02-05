df76e830c3382b4a1672a1116249193d8989bb4e
Arnulfotagsip <tagsiparnulfo26@gmail.com>
2026-02-05 13:03:39 +0800

    Modify first display page wt header/footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-import {Header} from "@/components/common/Header";
+import { Header } from "@/components/common/Header";
+import { Footer } from "@/components/common/Footer";
+
 export default function Home(){
-    return(
-        <div>
-            <Header />
-        </div>
-    )
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-1">
+      </main>
+      <Footer />
+    </div>
+  );
 }
