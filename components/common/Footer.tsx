93d0b12cb86959639ce01cf845de5b2ea8bd4c0e
Arnulfotagsip <tagsiparnulfo26@gmail.com>
2026-02-05 11:46:27 +0800

    Add page tsx from app and add info in footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,11 +1,38 @@
-export  function Footer(){
-return (
-    <footer className="bg-gray-500 text-white py-6 mt-12 ">
-        <div className="container mx-auto px-6 text-center">
-            <h1 className="font-bold text-lg">This is my footer</h1>
-         
-        </div>
-    </footer>
-
-)
+export  function Footer() {
+    return (
+        <footer className="bg-gray-500 text-white py-6 mt-12">
+            <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-10 text-sm">
+                {/* contact */}
+                <div  className="text-left">
+                    <h1 className="font-bold text-lg mb-2">Contact Me</h1>
+                    <ul className="italic flex flex-col gap-1">
+                        <li>Contact: 09162366938</li>
+                        <li>Gmail: Tagsiparnulfo26@gmail.com</li>
+                        <li>Facebook: Arnulfo Tagsip</li>
+                        <li>Github: Arnulfo Tagsip</li>
+                    </ul>
+                </div>
+                {/* projects */}
+                <div className="text-center">
+                    <h1 className="font-bold text-lg mb-2">Projects</h1>
+                    <ul className="italic flex flex-col gap-1">
+                        <li>Community Connect</li>
+                        <li>Garbage Monitoring</li>
+                        <li>Attendance Tracking</li>
+                        <li>Lost & Found</li>
+                    </ul>
+                </div>
+                {/* extra */}
+                <div className="text-right">
+                    <h1 className="font-bold text-lg mb-2">More Info</h1>
+                    <ul className="italic flex flex-col gap-1">
+                        <li>Contact</li>
+                        <li>Gmail</li>
+                        <li>Facebook</li>
+                        <li>Github</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
 }
